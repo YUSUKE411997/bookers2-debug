b8ba8ab27a39a87e1d097d4225ea52842ab62ed5
YUSUKE411997 <hy.skyblue.41@icloud.com>
2021-05-02 08:21:05 +0000

    [finish]comment

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
 	belongs_to :user
 	has_many :favorites, dependent: :destroy
+	has_many :book_comments, dependent: :destroy
 
 	validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
@@ -10,5 +11,6 @@ class Book < ApplicationRecord
   end
 	
 end
+
 
 
