f58c41c9d9138f605cc911f0612b1f6e9750b9a8
YUSUKE411997 <hy.skyblue.41@icloud.com>
2021-05-07 04:02:17 +0000

    [finish]prefecture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,6 @@ class ApplicationController < ActionController::Base
 
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email,:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email,:name, :postcode, :prefecture_name, :address_city, :address_street, :address_building])
   end
 end
